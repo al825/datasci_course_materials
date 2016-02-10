b15a6df89e73367a8036e29dfd6d8cc6964088aa
al825 <anqi.liang@yale.edu>
2016-02-09 22:22:42 -0500

    Update p1.sql

--- assignment2/p1.sql
+++ assignment2/p1.sql
@@ -18,8 +18,9 @@ SELECT term
 FROM frequency
 WHERE docid IN ('10398_txt_earn', '925_txt_trade') AND count=1;*/
 
-SELECT docid, COUNT (*)
-FROM frequency
-WHERE term LIKE '%parliament%'
-GROUP BY docid;
-
+SELECT COUNT(*)
+FROM 
+	(SELECT docid, COUNT (*)
+	 FROM frequency
+	 WHERE term LIKE '%parliament%'
+	 GROUP BY docid);
