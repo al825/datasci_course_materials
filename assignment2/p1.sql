20496bbc01fa9dc5b92aeae1c69e39b71a738bbf
al825 <anqi.liang@yale.edu>
2016-02-09 22:09:59 -0500

    Update p1.sql

--- assignment2/p1.sql
+++ assignment2/p1.sql
@@ -16,5 +16,5 @@ WHERE docid='925_txt_trade' AND count=1;*/
 
 SELECT term
 FROM frequency
-WHERE docid IN ('10398_txt_earn', '925_txt_trade') AND count=1 
+WHERE docid IN ('10398_txt_earn', '925_txt_trade') AND count=1;
 
