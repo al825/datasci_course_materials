11fcb6349466ef55df5e4e45f32a9aa410e7afae
al825 <anqi.liang@yale.edu>
2016-02-10 09:31:49 -0600

    Update p1.sql

--- assignment2/p1.sql
+++ assignment2/p1.sql
@@ -45,9 +45,10 @@ FROM
 	WHERE term = 'world');*/
 	
 
-SELECT *
+SELECT a.row_num,b.col_num, SUM(a.value*b.value)
 FROM a AS a,b AS b
-WHERE a.col_num=b.row_num;
+WHERE a.col_num=b.row_num
+GROUP BY a.row_num, b.col_num;
 
 
 
