51f51a134f0c9f528c2db925649a62c28277b20f
al825 <anqi.liang@yale.edu>
2016-02-09 22:13:46 -0500

    Update p1.sql

--- assignment2/p1.sql
+++ assignment2/p1.sql
@@ -12,9 +12,13 @@ WHERE docid='10398_txt_earn' AND count=1
 UNION 
 SELECT term
 FROM frequency
-WHERE docid='925_txt_trade' AND count=1;*/
+WHERE docid='925_txt_trade' AND count=1;
 
 SELECT term
 FROM frequency
-WHERE docid IN ('10398_txt_earn', '925_txt_trade') AND count=1;
+WHERE docid IN ('10398_txt_earn', '925_txt_trade') AND count=1;*/
+
+SELECT COUNT*
+FROM frequency
+WHERE term LIKE '%parliament%';
 
