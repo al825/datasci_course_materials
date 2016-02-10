ddb54c7b560f8ce7b6f603531ca24ab698580119
al825 <anqi.liang@yale.edu>
2016-02-09 22:41:36 -0500

    Update p1.sql

--- assignment2/p1.sql
+++ assignment2/p1.sql
@@ -34,11 +34,13 @@ FROM
 	 HAVING sums>300);*/
 	 
 
-SELECT *
+SELECT COUNT(DISTINCT docid)
+FROM
+	(SELECT *
 	FROM frequency
-	WHERE term ='transactions'
+	WHERE term = 'transactions'
 	INTERSECT
 	SELECT *
 	FROM frequency
-	WHERE term ='world';
+	WHERE term = 'world');
 
