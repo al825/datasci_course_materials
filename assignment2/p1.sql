434c3c3310c6381e9aed98de93bb4cd3dc889d51
al825 <anqi.liang@yale.edu>
2016-02-10 08:58:06 -0600

    Update p1.sql

--- assignment2/p1.sql
+++ assignment2/p1.sql
@@ -42,11 +42,22 @@ FROM
 	INTERSECT
 	SELECT docid
 	FROM frequency
-	WHERE term = 'world');*/
+	WHERE term = 'world');
 	
 
 SELECT a.row_num, b.col_num, SUM(a.value*b.value)
 FROM a AS a,b AS b
 WHERE a.row_num=b.col_num
-GROUP BY a.row_num, b.col_num;
+GROUP BY a.row_num, b.col_num;*/
+
+
+
+SELECT a.docid, b.docid, SUM(a.count*b.count)
+FROM frequency AS a, frequency AS b
+WHERE a.docid=b.docid AND a.term=b.term AND a.docid < b.docid
+GROUP BY a.docid, b.docid; 
+
+
+
+
 
