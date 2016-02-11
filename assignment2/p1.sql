cc0c69c73b4cfb2b0c06f5859401a47b74608be7
al825 <anqi.liang@yale.edu>
2016-02-10 23:13:51 -0500

    Update p1.sql

--- assignment2/p1.sql
+++ assignment2/p1.sql
@@ -53,9 +53,10 @@ GROUP BY a.row_num, b.col_num;*/
 
 
 
-SELECT a.docid, b.docid, a.term, b.term, SUM(a.count*b.count)
+SELECT a.docid, b.docid, a.term, b.term
 FROM frequency AS a, frequency AS b
-WHERE a.term=b.term AND a.docid ='9809_txt_acq' AND b.docid ='9810_txt_earn';
+WHERE a.term=b.term AND a.docid ='9809_txt_acq' AND b.docid ='9810_txt_earn'
+GROUP BY a.docid, b.docid; 
 
 
 /*CREATE VIEW temp AS 
