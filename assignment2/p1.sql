9fcaee63cd1f00813957664f94c6460783c57575
al825 <anqi.liang@yale.edu>
2016-02-10 23:17:41 -0500

    Update p1.sql

--- assignment2/p1.sql
+++ assignment2/p1.sql
@@ -53,7 +53,7 @@ GROUP BY a.row_num, b.col_num;*/
 
 
 
-SELECT a.docid, b.docid, a.term, b.term
+SELECT a.docid, b.docid, a.term, b.term, a.count, b.count
 FROM frequency AS a, frequency AS b
 WHERE a.term=b.term AND a.docid ='9809_txt_acq' AND b.docid ='9810_txt_earn';
 
